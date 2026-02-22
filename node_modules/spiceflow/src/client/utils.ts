1cbf02da695ffe0b8e307ed872d83197a78abae3
wildandwi.t.k <wildandwitriatnakusuma3915@gmail.com>
2026-02-22 20:00:45 +0700

    add .gitignore and remove node_modules from tracking

--- node_modules/spiceflow/src/client/utils.ts
+++
@@ -1,7 +0,0 @@
-export function parseStringifiedValue(value: string) {
-  try {
-    return JSON.parse(value)
-  } catch (error) {
-    return value
-  }
-}
--- node_modules/unframer/dist/exporter.test.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=exporter.test.d.ts.map
